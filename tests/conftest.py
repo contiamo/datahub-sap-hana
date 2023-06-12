96ad46a6dd996471557e059d6e9a72d0c75bfed4
Kay Alave <k.alave@gmail.com>
2023-06-12 17:51:07 +0200

    feat: add integration tests (#21)
    * feat: add notes on sap hana queries and file-based lineage yaml file
    
    * feat: add new datasource and annotations
    
    * fix: added logger.debug, removed references to internal sap hana dev, fixed white spaces
    
    * added new docker compose
    
    * feat: add integration tests
    
    * fixing the check_golden_file
    
    * feat: add integration tests
    
    * feat: after task fix
    
    * feat: adjust test for integration
    
    * feat: adjust test for integration, edited pypro and test_hana
    
    * feat: adjusted the decorators
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * adding new test and headers
    
    * changes to test_hana and other files
    
    * added headers to test_helpers and other files
    
    * Update ingestion.py to remove the constraints for SYS
    
    * Update datahub_sap_hana/ingestion.py
    
    Co-authored-by: Lucas Roesler <roesler.lucas@gmail.com>
    
    * Update datahub_sap_hana/ingestion.py
    
    Co-authored-by: Lucas Roesler <roesler.lucas@gmail.com>
    
    * remove the execution block in ingestion.py
    
    * Update hana_recipe.yaml to change sink type to console
    
    * Update README.md
    
    * removed query
    
    * Update hana_to_file.yml
    
    * Update the testing section
    
    * Update pyproject.toml
    
    Co-authored-by: Lucas Roesler <roesler.lucas@gmail.com>
    
    * Update notes.md
    
    * Update notes.md
    
    * Update docker-compose.yml
    
    * Update test_hana.py
    
    * Update docker-compose.yml
    
    * Update tests/README.md
    
    Co-authored-by: Lucas Roesler <roesler.lucas@gmail.com>
    
    * Update tests/docker-compose.yml
    
    Co-authored-by: Lucas Roesler <roesler.lucas@gmail.com>
    
    * changes to filenames, deleted notes.md
    
    * changes to filenames
    
    * changed file names
    
    * ran task fix
    
    * added deepdiff
    
    ---------
    
    Co-authored-by: kay_alave <kay.alave@gmail.com>
    Co-authored-by: Lucas Roesler <roesler.lucas@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,37 @@
 import logging
 import os
 
+import pytest
+
 # Enable debug logging.
 logging.getLogger().setLevel(logging.DEBUG)
 os.putenv("DATAHUB_DEBUG", "1")
 
 # Disable telemetry
 os.putenv("DATAHUB_TELEMETRY_ENABLED", "false")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--update-golden-files",
+        action="store_true",
+        default=False,
+    )
+    parser.addoption("--copy-output-files", action="store_true", default=False)
+
+
+def pytest_configure(config):
+    # Automatically skip integration tests if no mark is specified
+    markexpr = config.getoption("markexpr", None)
+    if not markexpr:
+        print("No markexpr specified, skipping integration tests")
+        markexpr = "not integration"
+
+    config.option.markexpr = markexpr
+
+
+def pytest_collection_modifyitems(items, config):
+    # Automatically mark tests as integration test if it starts with `test_integration`
+    for item in items:
+        if "test_integration" in item.nodeid:
+            item.add_marker(pytest.mark.integration)
