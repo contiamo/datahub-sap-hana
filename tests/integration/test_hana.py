d8eb30852d1adb7e29d4efb0bf46b9060fb93872
kay_alave <kay.alave@gmail.com>
2023-06-12 14:25:34 +0200

    changes to test_hana and other files

--- tests/integration/test_hana.py
+++ tests/integration/test_hana.py
@@ -1,3 +1,9 @@
+# This is copied and slightly modified from the acryl-datahub project
+# Copyright 2015 LinkedIn Corp. All rights reserved.
+#
+# The original source code can be found at https://github.com/datahub-project/datahub/blob/master/metadata-ingestion/tests/integration/hana/test_hana.py
+# And the original Apache 2 license is available at https://github.com/acryldata/datahub/blob/master/LICENSE
+
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
@@ -16,7 +22,7 @@ import yaml
 
 @pytest.fixture
 def hana_source():
-    with open("tests/integration/data/hana_to_file.yml") as f:
+    with open("tests/integration/data/hana_to_file_lineage_disabled.yml") as f:
         config_file = yaml.safe_load(f)
         config_dict = config_file["source"]["config"]
         ctx = PipelineContext(run_id="hana-test")
@@ -50,7 +56,6 @@ def test_integration_connection_with_query(hana_source: HanaSource):
 @pytest.mark.integration
 def test_integration_hana_ingest(pytestconfig):
     test_resources_dir = pytestconfig.rootpath / "tests/integration/data"
-
     # Run the metadata ingestion pipeline.
     config_file = (test_resources_dir / "hana_to_file.yml").resolve()
     run_datahub_cmd(
@@ -63,4 +68,24 @@ def test_integration_hana_ingest(pytestconfig):
         pytestconfig,
         output_path=test_resources_dir / "hana_mces.json",
         golden_path=test_resources_dir / "hana_mces_golden.json",
+    )
+
+
+# test for when the field include_view_lineage: false
+@pytest.mark.integration
+def test_integration_hana_ingest_lineage_disabled(pytestconfig):
+    test_resources_dir = pytestconfig.rootpath / "tests/integration/data"
+    # Run the metadata ingestion pipeline.
+    config_file = (test_resources_dir /
+                   "hana_to_file_lineage_disabled.yml").resolve()
+    run_datahub_cmd(
+        ["ingest", "--strict-warnings", "-c",
+            f"{config_file}"], test_resources_dir
+    )
+
+    # Verify the output.
+    mce_helpers.check_golden_file(
+        pytestconfig,
+        output_path=test_resources_dir / "hana_mces_lineage_disabled.json",
+        golden_path=test_resources_dir / "hana_mces_golden_lineage_disabled.json",
     )
