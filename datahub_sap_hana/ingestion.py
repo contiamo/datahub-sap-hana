77e218eeb904837676dc3afc6255bebc2b73d0ac
kay_alave <kay.alave@gmail.com>
2023-06-19 14:09:23 +0200

    chore: updates in taskfile, recipe files

--- datahub_sap_hana/ingestion.py
+++ datahub_sap_hana/ingestion.py
@@ -32,10 +32,10 @@ logger: logging.Logger = logging.getLogger(__name__)
 # Object dependencies in SAP HANA https://help.sap.com/docs/SAP_HANA_PLATFORM/de2486ee947e43e684d39702027f8a94/5ce9a6584eb84f10afbbf2b133534932.html
 LINEAGE_QUERY = """
 SELECT 
-    BASE_OBJECT_NAME as source_table, 
-    BASE_SCHEMA_NAME as source_schema,
-    DEPENDENT_OBJECT_NAME as dependent_view, 
-    DEPENDENT_SCHEMA_NAME as dependent_schema
+    LOWER(BASE_OBJECT_NAME) as source_table, 
+    LOWER(BASE_SCHEMA_NAME) as source_schema,
+    LOWER(DEPENDENT_OBJECT_NAME) as dependent_view, 
+    LOWER(DEPENDENT_SCHEMA_NAME) as dependent_schema
   from SYS.OBJECT_DEPENDENCIES 
 WHERE 
   DEPENDENT_OBJECT_TYPE = 'TABLE'
@@ -183,7 +183,7 @@ class HanaSource(SQLAlchemySource):
                     dependent_view,
                 ),
                 self.config.env,
-            ).lower()
+            )
 
             lineage_mce = mce_builder.make_lineage_mce(source_tables, urn)
             for item in mcps_from_mce(lineage_mce):
